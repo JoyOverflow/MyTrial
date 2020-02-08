29f168c6cfd30a2e9742cac49ed3384e1e2f5e90
joyoverflow <joyoverflow@sina.com>
2020-02-08 22:26:19 +0800

    进程信息

--- app/src/main/java/ouyj/hyena/com/mytrial/MainActivity.java
+++ app/src/main/java/ouyj/hyena/com/mytrial/MainActivity.java
@@ -1,7 +1,12 @@
 package ouyj.hyena.com.mytrial;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +21,24 @@ public class MainActivity extends AppCompatActivity {
 
         myLayout = findViewById(R.id.myLayout);
         myButton = findViewById(R.id.myButton);
+
+        int processId=android.os.Process.myPid();
+        String processName = getProcessName(getApplicationContext(),processId);
+        Log.d(TAG, String.format("进程ID：%d 进程名称：%s",processId,processName));
     }
+    public String getProcessName(Context cxt, int pid) {
+        ActivityManager am = (ActivityManager) cxt
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> runApps = am.getRunningAppProcesses();
+        if (runApps == null) return null;
+        for (ActivityManager.RunningAppProcessInfo info : runApps) {
+            if (info.pid == pid)
+                return info.processName;
+        }
+        return null;
+    }
+
+
+
+
 }
